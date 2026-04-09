36d69e26df87cf76418d57ac9a66944f01f2e17b
Swarup <swarupkst@gmail.com>
2026-04-09 23:03:00 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className='text-5xl text-center bg-amber-300 mx-110'>Setup done</h2>
+    </>
+  )
+}
+
+export default App
